ea5e624d47ac8398bfc5b766831fc7265f1a49e6
Matheus Lucheze Lopes <matlopes775@gmail.com>
2025-09-26 17:36:16 -0300

    results

--- Solutions/FMCBabyNat.hs
+++ Solutions/FMCBabyNat.hs
@@ -34,72 +34,109 @@ infixl 6 +
 
 -- Output: O means False, S O means True
 isZero :: Nat -> Nat
-isZero = undefined
+isZero O = S O
+isZero x = O
 
 -- pred is the predecessor but we define zero's to be zero
 pred :: Nat -> Nat
-pred = undefined
+pred (S x) = x
+pred zero = zero
 
 -- Output: O means False, S O means True
 even :: Nat -> Nat
-even = undefined
+even O = S O 
+even (S O) = O
+even (S (S x)) = even x
 
 odd :: Nat -> Nat
-odd = undefined
+odd O = O 
+odd (S O) = S O
+odd (S (S x)) = odd x
 
 -- This is called the dotminus or monus operator
 -- (also: proper subtraction, arithmetic subtraction, ...).
 -- It behaves like subtraction, except that it returns 0
 -- when "normal" subtraction would return a negative number.
 monus :: Nat -> Nat -> Nat
-monus = undefined
+monus O _ = O
+monus x O = x
+monus (S x) (S y) = monus x y
 
 (-*) :: Nat -> Nat -> Nat
 (-*) = monus
 
 -- multiplication
 (*) :: Nat -> Nat -> Nat
-(*) = undefined
+(*) _ * O = O
+(*) x * (S y) = x * y + x
 
 infixl 7 *
 
 -- exponentiation
 (^) :: Nat -> Nat -> Nat
-(^) = undefined
+(^) x O = one
+(^) x (S y) = x ^ y * x
 
--- decide: infix? ? ^
+infixr 8 ^
 
 -- quotient
 (/) :: Nat -> Nat -> Nat
-(/) = undefined
+(/) O x = O
+(/) x y = 
+  case y of 
+    O -> undefined
+    S z -> 
+      case monus x y of 
+        O -> 
+          case monus y x of 
+            O -> one
+            p -> O
+        z -> S ((x -* y) / y)     
+
+infixl 7 *
 
 -- remainder
 (%) :: Nat -> Nat -> Nat
-(%) = undefined
+(%) _ O = undefined
+(%) x y = x -* (y * (x / y))
 
 -- divides
 -- just for a change, we start by defining the "symbolic" operator
 -- and then define `devides` as a synonym to it
 -- again, outputs: O means False, S O means True
 (|||) :: Nat -> Nat -> Nat
-(|||) = undefined
-
+(|||) O _ = undefined
+(|||) (S x) O = S O
+(|||) (S x) y =
+				case y % S x of
+					O -> S O
+					z -> O
+        				
 -- x `absDiff` y = |x - y|
 -- (Careful here: this - is the actual minus operator we know from the integers!)
 absDiff :: Nat -> Nat -> Nat
-absDiff = undefined
+absDiff O O = O
+absDiff O x = x
+absDiff x O = x
+absDiff (S x) (S y) = absDiff x y
 
 (|-|) :: Nat -> Nat -> Nat
 (|-|) = absDiff
 
 factorial :: Nat -> Nat
-factorial = undefined
+factorial O = one
+factorial one = one
+factorial (S x) = factorial x * (S x)
 
 -- signum of a number (-1, 0, or 1)
 sg :: Nat -> Nat
-sg = undefined
+sg O = O
+sg S x = one
 
 -- lo b a is the floor of the logarithm base b of a
 lo :: Nat -> Nat -> Nat
-lo = undefined
-
+lo _ O = undefined
+lo O _ = undefined
+lo one x = undefined
+lo x one = O
+lo x y = S(lo x (y/x))
