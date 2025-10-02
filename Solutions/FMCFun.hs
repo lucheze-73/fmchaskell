0e3ef3f977c61e6f28adcd3f76c2ad4973f8b6c7
Matheus Lucheze Lopes <matlopes775@gmail.com>
2025-10-02 20:35:52 -0300

    all too fun

--- Solutions/FMCFun.hs
+++ Solutions/FMCFun.hs
@@ -12,28 +12,34 @@ import Prelude hiding
 
 -- curry takes a "traditional" binary function
 -- and returns its currified version
-curry :: undefined
-curry = undefined
+curry :: ((x, y)->z) -> x->y->z
+curry f x y = f (x,y)
 
 -- uncurry takes a currified function
 -- and returns its "traditional" binary version
-uncurry :: undefined
-uncurry = undefined
+uncurry :: (x->y->z) -> ((x, y)->z)
+uncurry f (x,y) = f x y
 
 -- flip takes a (currified) binary function
 -- and returns one that behaves the same but takes its arguments in the opposite order
+flip :: (x->y->z) -> y->x->z
+flip f x y = f y x
 
 -- (.) takes two composable functions and returns their composition
+(.) :: (x -> y) -> (y -> z) -> (x -> z)
+(f . g) x = g (f x)
 
 -- (.>) is composition but in diagramatic notation (should be ; but Haskell forbids)
 (.>) = flip (.)
 
 -- ($) takes a function and a suitable argument and applies the function to the argument
 -- think: why would we ever want that?
+($) :: (x -> y) -> x -> y
+f $ x = f x
 
 -- iterate: figure it out by its type
-iterate :: (a -> a) -> a -> [a]
-iterate = undefined
+iterate :: (x -> x) -> x-> [x]
+iterate f x = f x:iterate f (f x)
 
 -- orbit
 orbit = flip iterate
